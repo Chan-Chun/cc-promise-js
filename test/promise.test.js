24b5307c63e8b8b20cb1b379c9dfe7069c675484
Chan-Chun <362732414@qq.com>
2018-07-31 21:13:29 +0800

    adding promise-some

--- test/promise.test.js
+++ test/promise.test.js
@@ -1,31 +1,39 @@
-const { promiseAny } = require('../lib/promise')
+const { promiseAny, promiseSome } = require('../lib/promise')
 const assert = require('assert')
+
+const testArray = [
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(1)
+    }, 1000)
+  }),
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(2)
+    }, 2000)
+  }),
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(3)
+    }, 3000)
+  }),
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(4)
+    }, 4000)
+  })
+]
 
 describe('promise', () => {
   it('promise-any', () => {
-    promiseAny([
-      new Promise((resolve, reject) => {
-        setTimeout(() => {
-          reject(1)
-        }, 1000)
-      }),
-      new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve(2)
-        }, 2000)
-      }),
-      new Promise((resolve, reject) => {
-        setTimeout(() => {
-          reject(3)
-        }, 3000)
-      }),
-      new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve(4)
-        }, 4000)
-      })
-    ]).then(res => {
+    promiseAny(testArray).then(res => {
       assert.equal(res, 2)
+    })
+  })
+
+  it('promise-some', () => {
+    promiseSome(testArray, 2).then(res => {
+      assert.deepEqual(res, [2, 4])
     })
   })
 })
